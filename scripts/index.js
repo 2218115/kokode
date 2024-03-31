bf31a68a7ff1c0188f1f086da2a673d0d9fc8be0
2218115 <rindikid361@gmail.com>
2024-03-31 17:31:12 +0700

    Javascript I

---
+++ scripts/index.js
@@ -0,0 +1,93 @@
+window.onload = () => {
+    let indexGambarSekarang = 0;
+    let generatePlaceholderStatus = false;
+    let daftarGambar = [
+        "./images/carousel_1.png",
+        "./images/carousel_2.png",
+        "./images/carousel_3.png",
+    ];
+
+    const intervalGanti = 2000; // dalam ms
+    const carouselImageDom = document.getElementById("carousel-image");
+    const carouselStatusDom = document.getElementById("carousel-status");
+
+    function updateCaruouselStatus() {
+        if (!generatePlaceholderStatus) {
+            for (let i = 0; i < daftarGambar.length; ++i) {
+                const divStatus = document.createElement("div");
+                divStatus.classList.add("carousel__state");
+                if (i == indexGambarSekarang) {
+                    divStatus.classList.add("carousel__state--active");
+                }
+                carouselStatusDom.append(divStatus);
+            }
+
+            generatePlaceholderStatus = true;
+        } else {
+            const status = carouselStatusDom.children;
+            let index = 0;
+            for (const node of status) {
+                if (index != indexGambarSekarang) {
+                    node.classList.remove("carousel__state--active");
+                } else {
+                    node.classList.add("carousel__state--active");
+                }
+                index++;
+            }
+        }
+    }
+
+    updateCaruouselStatus();
+
+    carouselImageDom.onload = () => {
+        bookFlipSound.play();
+
+        indexGambarSekarang++;
+        if (indexGambarSekarang >= daftarGambar.length) {
+            indexGambarSekarang = 0;
+        }
+    }
+
+    function gantiGambar() {
+        const gambarSekarang = daftarGambar[indexGambarSekarang];
+        carouselImageDom.src = gambarSekarang;
+
+        updateCaruouselStatus();
+    }
+
+    let intervalId = setInterval(gantiGambar, intervalGanti);
+
+    const bookFlipSound = document.createElement("audio");
+    bookFlipSound.src = "./sounds/card_hover.wav";
+    carouselImageDom.onmouseover = () => {
+        if (intervalId != undefined) {
+            clearInterval(intervalId);
+            intervalId = undefined;
+        }
+    }
+
+    carouselImageDom.onmouseout = () => {
+        if (intervalId == undefined) {
+            intervalId = setInterval(gantiGambar, intervalGanti);
+        }
+    }
+
+    // mainkan suara jika ada event click pada links dan button
+    const clickSound = document.createElement("audio");
+    clickSound.src = "./sounds/click.wav";
+
+    const buttons = document.getElementsByTagName("button");
+    const anchors = document.getElementsByTagName("a");
+
+    for (const b of buttons) {
+        b.onclick = (e) => {
+            clickSound.play();
+        }
+    }
+
+    for (const a of anchors) {
+        a.onclick = (e) => {
+            clickSound.play();
+        }
+    }
+}
